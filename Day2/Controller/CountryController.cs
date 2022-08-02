8877fd9a50efa693580e092fcf55c60d3904a2a4
Akshata Ubale <61930998+akshata200@users.noreply.github.com>
2022-08-02 15:23:33 +0530

    Web API

---
+++ Day2/Controller/CountryController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("api/Country")]
+    public class CountryController : ApiController
+    {
+        static List<Country> countryList = new List<Country>()
+        {
+            new Country{ID=1,Name="India",Capital="Delhi"},
+            new Country{ID=2,Name="Japan",Capital="Tokyo"},
+            new Country{ID=3,Name="America",Capital="Washington DC"},
+            new Country{ID=4,Name="England",Capital="London"},
+        };
+
+        // GET Methods
+        [HttpGet]
+        public List<Country> Get()
+        {
+            return countryList;
+        }
+
+
+        [HttpGet]
+        [Route("{id}")]
+        public HttpResponseMessage GetCountryById(int id)
+        {
+            Country countryObj = countryList.Find(item => item.ID == id);
+            if(countryObj == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, id);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, countryObj);
+        }
+
+        [HttpGet]
+        [Route("Country/{name}")]
+        public HttpResponseMessage GetByCountryName(string name)
+        {
+            Country countryObj = countryList.Find(item => item.Name.Equals(name));
+            if (countryObj == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, name);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, countryObj);
+        }
+
+        [HttpGet]
+        [Route("Capital/{capital}")]
+        public HttpResponseMessage GetByCapitalName(string capital)
+        {
+            Country countryObj = countryList.Find(item => item.Capital.Equals(capital));
+            if (countryObj == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, capital);
+            }
+            return Request.CreateResponse(HttpStatusCode.OK, countryObj);
+        }
+
+        [HttpGet]
+        [Route("NameById/{id}")]
+        public IHttpActionResult GetCountryNameById(int id)
+        {
+            Country obj = countryList.Find(item => item.ID == id);
+            if (obj != null)
+            {
+                return Ok(obj.Name);
+            }
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        public List<Country> Post([FromBody] Country obj)
+        {
+            countryList.Add(obj);
+            return countryList;
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public IHttpActionResult Put([FromBody] Country newObj)
+        {
+            Country oldObj = countryList.Find(item => item.ID == newObj.ID);
+            if(oldObj != null)
+            {
+                int index = countryList.IndexOf(oldObj);
+                countryList.Remove(oldObj);
+                countryList.Insert(index, newObj);
+                return Ok();
+
+            }
+            return NotFound();
+            
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            Country obj = countryList.Find(item => item.ID == id);
+            if (obj != null)
+            {
+                bool isRemoved = countryList.Remove(obj);
+                if (isRemoved)
+                {
+                    return Ok(obj);
+                }
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ Day2/Controller/PersonController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [RoutePrefix("api/Person")]
+    public class PersonController : ApiController
+    {
+        static List<Person> personList = new List<Person>()
+        {
+            new Person{ID=1,Name="Smith",City="NJ"},
+            new Person{ID=2,Name="Dean",City="MI"},
+            new Person{ID=3,Name="Yisel",City="FL"},
+            new Person{ID=4,Name="Carlos",City="NJ"},
+        };
+
+        [HttpGet]
+        public List<Person> Get()
+        {
+            return personList;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Person GetPersonById(int id)
+        {
+            Person personObj = personList.Find(item => item.ID == id);
+            return personObj;
+        }
+
+        [HttpGet]
+        [Route("City/{city}")]
+        public IEnumerable<Person> GetAllPersonByCityName(string city)
+        {
+            IEnumerable<Person> cityList = personList.Where(item => item.City.Equals(city));
+            return cityList;
+        }
+
+        [HttpGet]
+        [Route("Name/{name}")]
+        public Person GetPersonByName(string name)
+        {
+            Person obj = personList.SingleOrDefault(item => item.Name.Equals(name));
+            return obj;
+        }
+
+        [HttpGet]
+        [Route("NameById/{id}")]
+        public IHttpActionResult GetPersonName(int id)
+        {
+            Person personObj = personList.Find(item => item.ID == id);
+            if (personObj != null)
+            {
+                return Ok(personObj.Name);
+            }
+            return NotFound();
+        }
+
+
+        [HttpPost]
+        public List<Person> Post([FromBody]Person obj)
+        {
+            personList.Add(obj);
+            return personList;
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public void Put([FromBody]Person newObj)
+        {
+            Person oldObj = personList.Find(item => item.ID == newObj.ID);
+            personList.Remove(oldObj);
+            personList.Add(newObj);
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            Person obj = personList.Find(item => item.ID == id);
+            if (obj != null)
+            {
+                bool isRemoved = personList.Remove(obj);
+                if (isRemoved)
+                {
+                    return Ok(obj);
+                }
+            }
+            return NotFound();
+        }
+
+    }
+}
