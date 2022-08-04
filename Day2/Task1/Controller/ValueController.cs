8d8706573217267e47beaf9a3496b028fb6c5564
Akshata Ubale <61930998+akshata200@users.noreply.github.com>
2022-08-04 09:47:21 +0530

    country web api

---
+++ Day2/Task1/Controller/ValueController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class ValueController : ApiController
+    {
+        static List<string> pracStr = new List<string>() {"Value1", "Value2", "Value3" };
+        public IEnumerable<string> Get()
+        {
+            return pracStr;
+        }
+        public string Get(int id)
+        {
+            return "valueSomething";
+        }
+
+        public IEnumerable<string> Post([FromBody]string val)
+        {
+            pracStr.Add(val);
+            return pracStr;
+        }
+    }
+}
